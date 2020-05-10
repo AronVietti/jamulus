95bc508de8b3748adb13519dd98681a9e025bcc4
Aron Vietti <aronvietti@gmail.com>
2020-05-10 16:51:42 -0700

    Adds Health Check to the Server
    Many services, like AWS, do basic TCP Health Checks by connecting
    to the socket. Create a HealthCheckSocket class that manages a
    TCP socket and it's connections. Added it to the HighPriority
    SocketThread and initialize it only for the Server.
    
    Created global value to control the max number of Health Check
    connections.
    
    So Health Check Connections and UDP messages can both be recieved
    on the same thread I made them non blocking. Used select to manage
    cpu usage by blocking when there is no new data or connection.
    
    Created socket error handling code into it's own file.
    
    Added Socket helper functions to the Socket implementations. While
    nice to encapulate functionanlity, this was mostly to make the ifdef
    for windows cleaner.

--- src/healthcheck.cpp
+++ src/healthcheck.cpp
@@ -23,195 +23,31 @@
 \******************************************************************************/
 
 #include "healthcheck.h"
+#include "socket.h"
+#include "socketerrors.h"
 #include <string>
-#include <unistd.h>
 #include <fcntl.h>
 #include "global.h"
+#include <QString>
+#include <QTextStream>
+#include "util.h"
 
-/* Socket Helper Functions ****************************************************/
-
-// Handle Socket Errors for Posix and Windows
-static void HandleSocketError(int error)
-{
-    switch (errno)
-    {
-#ifdef _WIN32
-    case WSANOTINITIALISED:
-        throw CGenErr("HealthCheck: A successful WSAStartup call must occur before using this function.", "Network Error");
-
-#endif
-#ifndef _WIN32
-    case EACCES:
-#endif
-        throw CGenErr("HealthCheck: The address is protected, and the user is not the superuser.", "Network Error");
-
-#ifndef _WIN32
-    case EADDRINUSE:
-#else
-    case WSAEADDRINUSE:
-    case WSAEISCONN:
-#endif
-        throw CGenErr("HealthCheck: The given address is already in use.", "Network Error");
-
-
-#ifndef _WIN32
-    case EBADF:
-#else
-    case WSAENOTCONN:
-    case WSAENETRESET:
-    case WSAESHUTDOWN:
-#endif
-        throw CGenErr("HealthCheck: not a valid file descriptor or is not open for reading.", "Network Error");
-
-#ifndef _WIN32
-    case EINVAL:
-#else
-    case WSAEINVAL:
-#endif
-        throw CGenErr("HealthCheck: The socket is already bound to an address, or addrlen is wrong, \
-            or addr is not a valid address for this socket's domain. ", "Network Error");
-
-#ifndef _WIN32
-    case ENOTSOCK:
-#else
-    case WSAENOTSOCK:
-#endif
-        throw CGenErr("HealthCheck: The file descriptor sockfd does not refer to a socket.", "Network Error");
-
-#ifndef _WIN32
-    case EOPNOTSUPP:
-#else
-    case WSAEOPNOTSUPP:
-#endif
-        throw CGenErr("HealthCheck: The socket is not of a type that supports the listen() operation.", "Network Error");
-
-#ifndef _WIN32
-    case EFAULT:
-#else
-    case WSAEFAULT:
-#endif
-        throw CGenErr("HealthCheck: buf is outside your accessible address space.", "Network Error");
-
-#ifndef _WIN32
-    case EINTR:
-#else
-#endif
-        throw CGenErr("HealthCheck: The call was interrupted by a signal before any data was read.", "Network Error");
-
-#ifndef _WIN32
-    case EIO:
-#else
-#endif
-        throw CGenErr("HealthCheck: I/O error.  This will happen for example when the process is \
-            in a background process group, tries to read from its \
-            controlling terminal, and either it is ignoring or blocking \
-            SIGTTIN or its process group is orphaned.It may also occur \
-            when there is a low - level I / O error while reading from a disk \
-            or tape.A further possible cause of EIO on networked \
-            filesystems is when an advisory lock had been taken out on the \
-            file descriptor and this lock has been lost.See the Lost \
-            locks section of fcntl(2) for further details.", "Network Error");
-
-#ifdef _WIN32
-    case WSAENOBUFS:
-        throw CGenErr("HealthCheck: No buffer space is available.", "Network Error");
-
-    case WSAEMFILE:
-        throw CGenErr("HealthCheck: No more socket descriptors are available.", "Network Error");
-
-    case WSAENETDOWN:
-        throw CGenErr("HealthCheck: The network subsystem has failed.", "Network Error");
-
-#endif
-    default:
-        throw CGenErr("HealthCheck: Socket error # " + std::to_string(error), "Network Error");
-    }
-}
-
-static int GetError()
-{
-#ifdef _WIN32
-    return WSAGetLastError();
-#else
-    return errno;
-#endif
-}
-
-static bool IsNonBlockingError(int error)
-{
-#ifdef _WIN32
-    return error == WSAEINPROGRESS || error == WSAEWOULDBLOCK;
-#else
-    return error == EAGAIN || error == EWOULDBLOCK;
-#endif
-}
-
-static bool IsDisconnectError(int error)
-{
-#ifdef _WIN32
-    return error == WSAENOTCONN || error == WSAENETRESET || error == WSAESHUTDOWN;
-#else
-    return error == EBADF;
-#endif
-}
-
-#ifdef _WIN32
-static bool SocketConnected(Socket socket)
-#else
-static bool SocketConnected(int socket)
-#endif
-{
-    int result = 0;
-    char fakeBuffer;
-
-#ifdef _WIN32
-    result = recv(socket, &fakeBuffer, 1);
-#else
-    result = read(socket, &fakeBuffer, 1);
-#endif
-
-    // A 0 result means Socket was succesfully disconnected
-    if (result == 0)
-        return true;
-    // An error was returned, handle the error
-    if (result == -1)
-    {
-        // If the error was one of the Non Blocking codes we're still connected
-        int error = GetError();
-        if (IsNonBlockingError(error))
-            return true;
-        if (IsDisconnectError(error))
-            return false;
-
-        HandleSocketError(error);
-    }
-}
-
-#ifdef _WIN32
-static void CloseSocket(Socket socket)
-{
-    closesocket(socket);
-}
-#else
-static void CloseSocket(int socket)
-{
-    close(socket);
-}
-#endif
+using namespace SocketError;
 
 /* Classes *******************************************************************/
 
 CHealthCheckSocket::CHealthCheckSocket(const quint16 iPortNumber)
+    : bListening(false)
 {
     Init(iPortNumber);
 }
 
 CHealthCheckSocket::~CHealthCheckSocket()
 {
-    AcceptThread.Stop();
+    Close();
 }
 
-CHealthCheckSocket::Init(const quint16 iPortNumber)
+void CHealthCheckSocket::Init(const quint16 iPortNumber)
 {
 #ifdef _WIN32
     // for the Windows socket usage we have to start it up first
@@ -224,64 +60,59 @@ CHealthCheckSocket::Init(const quint16 iPortNumber)
 
     TcpSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Try to set this socket as non blocking. This makes it easier to accept and manage
-    // connections on a single thread.
-    int blocking = 0;
-
-#ifdef _WIN32
-    unsigned long mode = 1;
-    blocking = ioctlsocket(fd, FIONBIO, &mode) == 0) ? true : false;
-#else
-    int flags = fcntl(fd, F_GETFL, 0);
-
-    if (flags == -1) blocking = flags;
-    else
+    if(!SetNonBlocking(TcpSocket))
     {
-        flags = flags | O_NONBLOCK;
-        blocking = fcntl(fd, F_SETFL, flags);
-    }
-#endif
-
-    if(blocking = -1)
         HandleSocketError(GetError());
+        return;
+    }
 
     sockaddr_in TcpSocketInAddr;
 
     TcpSocketInAddr.sin_family = AF_INET;
-    TcpSocketInAddr.sin_addr = htonl(INADDR_ANY);
+    TcpSocketInAddr.sin_addr.s_addr = INADDR_ANY;
     TcpSocketInAddr.sin_port = htons(iPortNumber);
 
-    int bound = bind(TcpSocket, &TcpSocketInAddr, sizeof(TcpSocketInAddr));
-
-    if (bound == -1)
+    if (!BindSocket(TcpSocket, TcpSocketInAddr))
         HandleSocketError(GetError());
 }
 
-CHealthCheckSocket::Listen()
+#ifdef _WIN32
+    const SOCKET &CHealthCheckSocket::Socket()
+#else
+    const int &CHealthCheckSocket::Socket()
+#endif
+{
+    return TcpSocket;
+}
+
+void CHealthCheckSocket::Listen()
 {
     int listening = listen(TcpSocket, 0);
 
     if (listening == -1)
+    {
         HandleSocketError(GetError());
+        return;
+    }
 
-    AcceptThread.SetSocket(this);
-
-    moveToThread(&AcceptThread);
+    bListening = true;
 }
 
 #ifdef _WIN32
 SOCKET CHealthCheckSocket::Accept()
 {
+    int addrlen;
 #else
 int CHealthCheckSocket::Accept()
 {
+    socklen_t addrlen;
 #endif
     sockaddr addr;
-    socklen_t addrlen;
+
     return accept(TcpSocket, &addr, &addrlen);
 }
 
-CHealthCheckSocket::Close()
+void CHealthCheckSocket::Close()
 {
 #ifdef _WIN32
     // closesocket will cause recvfrom to return with an error because the
@@ -294,89 +125,68 @@ CHealthCheckSocket::Close()
     // on Linux the shutdown call cancels the recvfrom
     shutdown(TcpSocket, SHUT_RDWR);
 #endif
+
+    bListening = false;
 }
 
-CHealthCheckSocket::CHealthCheckThread::CHealthCheckThread(CHealthCheckSocket* pNewSocket, QObject* parent)
-    : QThread(parent), pSocket(pNewSocket), bRun(false)
-{}
-
-void CHealthCheckSocket::CHealthCheckThread::Stop()
+void CHealthCheckSocket::HandleConnections()
 {
-    bRun = false;
+    if(!bListening) return;
 
-    pSocket->Close();
-
-    for (auto connection = ConnectionSockets.cbegin(); connection != ConnectionSockets.cend(); ++connection)
-    {
-        CloseSocket(*connection);
-        connection = ConnectionSockets.erase(connection);
-    }
-
-    wait(5000);
-}
-
-void CHealthCheckSocket::CHealthCheckThread::SetSocket(CHealthCheckSocket* pNewSocket)
-{
-    pSocket = pNewSocket;
-}
-
-
-
-void CHealthCheckSocket::CHealthCheckThread::run()
-{
-    bRun = true;
-
-    while (pSocket != nullptr && bRun)
-    {
 #ifdef _WIN32
-        SOCKET newConnection;
+        SOCKET newConnection = Accept();
 #else
-        int newConnection;
+	    int newConnection  = Accept();
 #endif
-        newConnection = pSocket.Accept();
-
+        
+#ifdef _WIN32
+        if (newConnection == INVALID_SOCKET)
+        {
+#else
         if (newConnection == -1)
         {
+#endif
             int error = GetError();
+
             if (!IsNonBlockingError(error))
             {
-                bRun = false;
-                connected = false;
+                Close();
                 HandleSocketError(error);
-                break;
+                return;
             }
         }
-            
-        ConnectionSockets.push_back(newConnection);
+	    else
+	    {
+            bool nonblocking = SetNonBlocking(newConnection);
 
+            if(!nonblocking)
+                HandleSocketError(GetError());
+
+            vecConnectionSockets.push_back(newConnection);
+	    }
+        
         // Check existing connections. If they're closed then remove them
-        for (auto connection = ConnectionSockets.cbegin(); connection != ConnectionSockets.cend(); ++connection)
-        {   
+        for (auto connection = vecConnectionSockets.cbegin(); connection != vecConnectionSockets.cend();)
+        {  
             if (!SocketConnected(*connection))
             {
                 CloseSocket(*connection);
-                connection = ConnectionSockets.erase(connection);
+                connection = vecConnectionSockets.erase(connection);
             }
+            else
+                ++connection;
         }
 
         // Make sure we don't have too many connections.
         //Disconnect and remove the oldest one if we do.
-        if (ConnectionSockets.size() > MAX_NUM_HEALTH_CONNECTIONS)
+        if (vecConnectionSockets.size() > MAX_NUM_HEALTH_CONNECTIONS)
         {
 #ifdef _WIN32
-            SOCKET& oldConnection;
+            SOCKET& oldConnection = vecConnectionSockets.front();
 #else
-            int& oldConnection;
+            int& oldConnection = vecConnectionSockets.front();
 #endif
-            oldConnection = ConnectionSockets.front();
-            ConnectionSockets.erase(ConnectionSockets.cbegin());
+            vecConnectionSockets.erase(vecConnectionSockets.cbegin());
             CloseSocket(oldConnection);
         }
-
-        // Because the socket is set to not block this loop could peg the cpu.
-        // Putting in a smal wait to prevent that.
-        wait(5);
-    }
-
-    bRun = false;
 }
